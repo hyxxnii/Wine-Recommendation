90a92c689d34be8835b8f51bfe8dc861e03f1faf
Yeonghyeon Kim <khyeon0819@gmail.com>
2022-05-24 15:47:46 +0900

    Add app.py templates static

---
+++ app.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import json
+from module import *
+from flask import Flask, request, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("main.html")
+
+@app.route("/main", methods=['GET', 'POST'])
+def main_get(name=None):
+    if request.method == "POST":
+        name = request.form["name"]
+        return show_result(name)
+    else:
+        return render_template("main.html")
+
+@app.route("/show_result", methods=['GET', 'POST'])
+def show_result(name):
+    # result_dict = food_recommendation(name)
+    # if request.method == "POST":
+    #     return render_template('result.html', json.dumps(result_dict))
+    # 테스트
+    if request.method == "POST":
+        return render_template('result.html', name)
+
+@app.route('/show_result')
+def about():
+    return render_template('result.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
